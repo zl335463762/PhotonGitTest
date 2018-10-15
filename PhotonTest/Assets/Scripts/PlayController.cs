732c0d0ea5e11989290c256b4b1598aeb354ee05
zl2290103097 <2290103097@qq.com>
2018-10-15 10:59:04 +0800

    创建

---
+++ PhotonTest/Assets/Scripts/PlayController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayController : MonoBehaviour {
+
+    public GameObject cube;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void BiggerButtonAction()
+    {
+        Debug.Log("变大按钮");
+        cube.transform.localScale = new Vector3(3, 3, 3);
+    }
+
+    public void SmallerButtonAction()
+    {
+        Debug.Log("变小按钮");
+        cube.transform.localScale = new Vector3(1, 1, 1);
+    }
+
+}
